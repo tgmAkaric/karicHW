bc3628a75ae735ad639515a1c9f240814391b6f2
Philipp Adler <padler@student.tgm.ac.at>
2014-02-28 14:13:54 +0100

    Hinzufügen von Adlers Klassen

---
+++ src/GrussMethoden.java
@@ -0,0 +1,13 @@
+/**
+ * Diese Klasse schreibt schöne grüße vom Projektteilnehmer
+ * @author Philipp Adler
+ * @version 2014-02-28
+ */
+public class GrussMethoden {
+	/**
+	 * Diese Methode wünscht schöne Grüße von dem Projektteilnehmer tgm1314-padler
+	 */
+	public static void adlerwünscht(){
+		System.out.println("Schöne Grüße von tgm1314-padler");
+	}
+}
