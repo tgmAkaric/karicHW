1554cb5ef36952cb9d2320e486797e22028e7d4b
Jakub Kopec <jkopec@student.tgm.ac.at>
2014-03-07 09:09:03 +0100

    Jakub hat hinzugefügt

---
+++ src/MainKlasse.java
@@ -0,0 +1,16 @@
+/**
+ * Diese Klasse benutzt die GrussMethoden klasse welche grüße vom Teilnehmer ausgibt
+ * @author Philipp Adler
+ * @version 2014-02-28
+ */
+public class MainKlasse {
+	/**
+	 * In der Main Methode werden die Aussagen ausgegeben
+	 * @param args ein Text-Array Objekt
+	 */
+	public static void main(String[] args) {
+		GrussMethoden.adlerwünscht();//Diese Methode stammt von Adler
+		GrussMethoden.andiwuenscht();//Diese Mehthode stammt von Andi
+		GrussMethoden.jakubwuenscht();//Diese Mehthode stammt von Jakub
+	}
+}
